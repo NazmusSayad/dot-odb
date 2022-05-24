fe99fabb63123c8f7816fb6925caf5584b6399b0
Nazmus Sayad <247sayad@gmail.com>
2022-05-24 16:11:09 +0600

    file added

---
+++ ODB.js
@@ -0,0 +1,40 @@
+const ODB = {
+  json: function (input) {
+    input = input.replace(/\r/gim, "")
+
+    const firstArray = input.split("\n\n")
+    const header = firstArray.shift().split(",")
+
+    const finalData = []
+
+    firstArray.filter((current) => {
+      if (current === "") return
+
+      current = current.split("\n")
+      const objectToPush = {}
+
+      header.forEach((key, index) => {
+        let calculatedData = current[index]
+
+        if (calculatedData === undefined) {
+          calculatedData = ""
+        } else {
+          calculatedData = calculatedData.startsWith(";") ? calculatedData.slice(1) : calculatedData
+        }
+
+        if (calculatedData) {
+          objectToPush[key.trim()] = calculatedData
+        }
+      })
+      if (Object.keys(objectToPush).length) {
+        finalData.push(objectToPush)
+      }
+    })
+
+    return finalData
+  },
+  jsonFile: async function (url) {
+    const file = await (await fetch(url)).text()
+    return ODB.json(file)
+  },
+}
